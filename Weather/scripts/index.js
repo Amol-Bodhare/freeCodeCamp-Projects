2ef2c1eb417eb3dcae30e5dc777902e0de8e3ed9
unknown <amolfeb95@gmail.com>
2018-03-30 23:01:23 -0400

    Revert "Final"
    This reverts commit e68145b29e8e6906f5e4e0748cc8603263e59d23.

--- Weather/scripts/index.js
+++ Weather/scripts/index.js
@@ -1,114 +1,179 @@
- obj = {
-   lat:null,
-   long:null,
- }
-data=null;
-weekdays=["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
-months=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"];
-function getLocation(){
-  var list = document.getElementById("heading1");
-  if (navigator.geolocation) {
-       var lat;
-          console.log("in");
-     getPreciseLocation()
-    .then(displayData)
-    .then(getWeatherInfo)
-    .then(renderData);
-    } else {
-        console.log("Error");
-        list.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-function getPreciseLocation() {
-  return new Promise(function (resolve, reject) {
-    console.log("er");
-    var options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 6000000
-};
-    navigator.geolocation.getCurrentPosition(function (position) {
-      console.log("position: "+position.coords.latitude);
-      resolve([position.coords.latitude, position.coords.longitude]);
-      
-    },function(err){
-      console.log("errrrr");
-      console.log(err);
-    },options);
-  });
-}
+ /**
+  * Created by Amol!
+  */
+var init =function (){
+  /**
+   * Global Object to store Latitude and Longitude values.
+   */
+  coordinateObject = {
+    lat:null,
+    long:null,
+  }
 
-function displayData(lat){
-  console.log("longitude is:"+lat);
-  obj['lat']=lat[0];
-  obj['long']=lat[1];
-  console.log("longggg"+obj['lat']+" "+obj['long']);
-}
-function getWeatherInfo(){
-  console.log("getWeather"+obj['lat']+" "+obj['long']);
-  var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://fcc-weather-api.glitch.me/api/current?lat="+obj['lat'].toFixed(2)+"&lon="+obj['long'].toFixed(2)+"", false);
-xhr.onload = function () {
-  // Begin accessing JSON data here
-  data = JSON.parse(this.response);
- if (xhr.status >= 200 && xhr.status < 400 && data['coord']['lat']==obj['lat'].toFixed(2) && data['coord']['lon']==obj['long'].toFixed(2)) {
-    obj['temp']=data['main']['temp'];
-    obj['icon']=data['weather'][0]['icon'];
-   console.log("icon:"+obj['icon']);
-  } else {
-    getWeatherInfo();
-    console.log('resent= '+data['coord']['lat']+" "+data['coord']['lon']);
+  /**
+   * Global variable to store Json object from API response.
+   */
+  responseData=null;
+
+  /**
+   * Hard-coded variable to convert integer to corresponding 
+   * Day and Month.
+   */
+  weekdays=["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
+  months=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"];
+
+
+  /**
+   *  onLoad
+   */
+  function onLoad(){
+    var list = document.getElementById("heading1");
+    console.log(navigator.geolocation);
+    $( "#headin" ).click(function() {
+      alert( "Handler for .click() called." );
+    });
+        var cordinates;
+            console.log("in");
+      getPreciseLocation()
+      .then(storeLocation)
+      .then(getWeatherInfo)
+      .then(renderData);
+    
   }
+  /**
+  * Get geoLocation
+  */
+  function getPreciseLocation() {
+    return new Promise(function (resolve, reject) {
+    
+      /**
+       * Object options containing properties to pass as a parameter 
+       * of Geolocation.getCurrencPosition()
+       */
+      var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 6000000
+      };
+
+      /**
+       * Get current position
+       */
+      navigator.geolocation.getCurrentPosition(function (position) {
+          return resolve([position.coords.latitude, position.coords.longitude]);
+        },function(err){
+            console.log(err);
+          },
+      options);
+    });
+  }
+
+  /**
+   * Stores Cordinates in CordinateObject.
+   * @param {Array} cordinates  
+   */
+  function storeLocation(cordinates){
+    
+    coordinateObject['lat']=cordinates[0];
+    coordinateObject['long']=cordinates[1];
+    console.log("latitude"+coordinateObject['lat']+" Longitude"+coordinateObject['long']);
+  }
+
+  /**
+   * Gets Weather Information based on Cordinates
+   */
+  function getWeatherInfo(){
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://fcc-weather-api.glitch.me/api/current?lat="+coordinateObject['lat'].toFixed(2)+"&lon="+coordinateObject['long'].toFixed(2)+"", false);
+    xhr.onload = function () {
+    // Begin accessing JSON responseData here
+      responseData = JSON.parse(this.response);
+      if (xhr.status >= 200 && xhr.status < 400 && responseData['coord']['lat']==coordinateObject['lat'].toFixed(2) && responseData['coord']['lon']==coordinateObject['long'].toFixed(2)) {
+        coordinateObject['temp']=responseData['main']['temp'];
+        coordinateObject['icon']=responseData['weather'][0]['icon'];
+        console.log("icon:"+coordinateObject['icon']);
+      } else {
+        getWeatherInfo();
+        console.log('resent= '+responseData['coord']['lat']+" "+responseData['coord']['lon']);
+      } 
+    }
+    xhr.send();
+    
+  }
+
+  /**
+   * True: Show temperature in celsius
+   * False: Show temperature in farheneit
+   */
+  degreeCelsius=true;
+
   
-}
-  xhr.send();
-  console.log(xhr.status);
-  console.log("temp"+obj['temp']);
-console.log(xhr.statusText);
-}
-degreeCelsius=true;
-function changeTemperature(){
-  if(degreeCelsius){
-    degreeCelsius=false;
-  }
-  else if(!degreeCelsius){
-    degreeCelsius=true;
-  }
-  
-  renderData();
-}
-function renderData(){
-  if(data['main']['temp']!=null){
+  /**
+   * Changes boolean value of degreeCelsius, 
+   * if user clicks on temperature HTML element.
+   */
+  function changeTemperature(){
     var displayTemp = document.getElementById("displayTemperature");
-    var degree = document.getElementById("degree");
-    console.log("temp"+obj['temp']);
+    console.log("inside")
     if(degreeCelsius){
-      displayTemp.innerHTML = Math.round(data['main']['temp']);
-      degree.innerHTML = "C";
+      var degreeFarheniet=(responseData['main']['temp']*1.8)+32;
+        console.log("far"+degreeFarheniet);
+        displayTemp.innerHTML=Math.round(degreeFarheniet);
+        degree.innerHTML = "&#176; F";
+      degreeCelsius=false;
     }
-    else{
-      var degreeFarheniet=(data['main']['temp']*1.8)+32;
-      console.log("far"+degreeFarheniet);
-      displayTemp.innerHTML=Math.round(degreeFarheniet);
-      degree.innerHTML = "F";
+    else if(!degreeCelsius){
+      degreeCelsius=true;
     }
-  if(data['weather'][0]['icon']!=undefined && data['weather'][0]['icon']!=null){
-    var date = new Date();
-    var modulus;
-    var hours=0;
-    if(date.getHours()<=12){
-      modulus="AM";
-      hours=date.getHours();
-    }
-    else{
-      modulus="PM";
-      hours=date.getHours()%12;
-    }
-    document.getElementById("time").innerHTML = hours+":"+date.getMinutes()+" "+modulus;
-    document.getElementById("date").innerHTML = weekdays[date.getDay()]+", "+date.getDate()+" "+months[date.getMonth()];
-    document.getElementById("description").innerHTML = data['weather'][0]['description'];
-    document.getElementById("icon-image").src = obj['icon']; 
-    document.getElementById("heading2").innerHTML = data['name']+" , "+data['sys']['country'];
+    
   }
+
+  /**
+   * Renders Weather data
+   */
+  function renderData(){
+    if(responseData['main']['temp']!=null){
+      var displayTemp = document.getElementById("displayTemperature");
+      var degree = document.getElementById("degree");
+      // console.log("temp"+coordinateObject['temp']);
+      if(degreeCelsius){
+        displayTemp.innerHTML = Math.round(responseData['main']['temp']);
+        degree.innerHTML = "&#176; C";
+      }
+      else{
+        var degreeFarheniet=(responseData['main']['temp']*1.8)+32;
+        console.log("far"+degreeFarheniet);
+        displayTemp.innerHTML=Math.round(degreeFarheniet);
+        degree.innerHTML = "&#176; F";
+      }
+    if(responseData['weather'][0]['icon']!=undefined && responseData['weather'][0]['icon']!=null){
+      var date = new Date();
+      var modulus;
+      var hours=0;
+      if(date.getHours()<=12){
+        modulus="AM";
+        hours=date.getHours();
+      }
+      else{
+        modulus="PM";
+        hours=date.getHours()%12;
+      }
+      document.getElementById("time").innerHTML = hours+":"+date.getMinutes()+" "+modulus;
+      document.getElementById("date").innerHTML = weekdays[date.getDay()]+", "+date.getDate()+" "+months[date.getMonth()];
+      document.getElementById("description").innerHTML = responseData['weather'][0]['description'];
+      document.getElementById("icon-image").src = coordinateObject['icon']; 
+      document.getElementById("heading2").innerHTML = responseData['name']+" , "+responseData['sys']['country'];
+    }
+    }
   }
+  
+  onLoad();
+ 
 }
+//init.changeTemperature();
+function change(){
+  console.log("lol");
+ console.log(init.degreeCelsius);
+}
+init();
