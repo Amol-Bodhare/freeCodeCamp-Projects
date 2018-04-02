3a8661aec668bd8cfe49afbf25e3a822ee0ce0de
unknown <amolfeb95@gmail.com>
2018-04-02 17:11:27 -0400

    Final Commit

--- TwitchTV/scripts/index.js
+++ TwitchTV/scripts/index.js
@@ -1,91 +1,98 @@
 /**
  * Created by Amol!
  */
-index=0;
- streamsObject ={
-    "ESL_SC2":{status:""},
-    "OgamingSC2":{status:""},
-    "cretetion":{status:""}, 
-    "freecodecamp":{status:""},
-    "habathcx":{status:""},
-    "RobotCaleb":{status:""},
-    "noobs2ninjas":{status:""},
-    "capcomfighters":{status:""}
+/**
+ * streamsObject: Object variable to store status of streams
+ * Status initialized as empty string.
+ */
+streamsObject = {
+    "ESL_SC2": { status: "" },
+    "OgamingSC2": { status: "" },
+    "cretetion": { status: "" },
+    "freecodecamp": { status: "" },
+    "habathcx": { status: "" },
+    "RobotCaleb": { status: "" },
+    "noobs2ninjas": { status: "" },
+    "capcomfighters": { status: "" }
 }
 
-function onLoad(){
-    document.getElementById("head").innerHTML="heading";
-    
-    apiCall().then(display);
+/**
+ * OnLoad initializes the execution of JavaScript
+ */
+function onLoad() {
+
+    apiCall();
 
 }
-function apiCall(){
+/**
+ * function responsible for requesting data from Twicth API 
+ */
+function apiCall() {
     return new Promise((resolve, reject) => {
         var keys = Object.keys(streamsObject);
-        for(var i=0;i<keys.length;i++){
-            console.log("before")
+        for (var i = 0; i < keys.length; i++) {
+
             var script = document.createElement("script");
-            script.src="https://wind-bow.glitch.me/twitch-api/streams/"+keys[i]+"?&callback=foo";
+            script.src = "https://wind-bow.glitch.me/twitch-api/streams/" + keys[i] + "?&callback=foo";
             document.body.appendChild(script);
-            //document.body.removeChild(script);
-            console.log("after");
+
+
         }
-        //console.log(JSON.stringify(streamsObject));
-        resolve();
-        //reject(console.log("err"));
+
+
     });
 }
 
-function display(data){
-    console.log(data);
-    // var keys = Object.keys(data);
-    // for(var i=0;i<keys.length;i++){
-    //     var listItem = document.createElement("li");
-
-    //     listItem.innerHTML = keys[i] + " is " + data[keys[i]]['status'];
-    //     id("list").appendChild(listItem);
-    // }
-}
-function foo(obj){
-   console.log("foo");
+/**
+ * Callback function  
+ * 
+ * @param {JSON Object} obj : contains API response.
+ */
+function foo(obj) {
+    console.log("foo");
     var url = "https://api.twitch.tv/kraken/streams/";
     var streamName = obj['_links']['self'].split(url);
-       
-        //var listItem = document.createElement("li");
-    if(obj['stream']===null){
-       
+
+
+    if (obj['stream'] === null) {
+
         streamsObject[streamName[1]]['status'] = "Offline";
-        id(streamName[1]).setAttribute("style","color:yellow");
-       // id("list").appendChild(listItem);
-        
-    }else{
+        id(streamName[1]).setAttribute("style", "color:yellow");
+
+
+    } else {
         streamsObject[streamName[1]]['status'] = "Online";
-        id(streamName[1]).setAttribute("style","color:#004d00");
+        id(streamName[1]).setAttribute("style", "color:#004d00");
         console.log(obj);
-        id(streamName[1]+"-span").innerHTML = obj['stream']['channel']['status'];
-        // listItem.innerHTML = streamName[1]+" is Online";
-        // var para = document.createElement("p");
-        // para.innerHTML = (obj['stream']['channel']['status']);
-        // id("list").appendChild(listItem);
-        // //listItem.setAttribute("id","l1");
-        // listItem.appendChild(para);
+        id(streamName[1] + "-span").innerHTML = obj['stream']['channel']['status'];
+
     }
     var listItem = document.getElementById(streamName[1]);
-   // var icon = document.getElementById(streamName[1]+"icon");
-   // icon =
+
     listItem.innerHTML = streamsObject[streamName[1]]['status'];
-        //id("list").appendChild(listItem);
-   // resolve(streamsObject);
-    //console.log(JSON.stringify(streamsObject));
 
 }
-function id(id){
+
+/**
+ * returns document.getElementByID();
+ * @param {var} id : contains ID of HTML element 
+ */
+function id(id) {
     return document.getElementById(id);
 }
-setInterval(function() {
+
+/**
+ * Timeout function to refresh page
+ * Time interval: 5 sec.
+ */
+setInterval(function () {
     // method to be executed;
-    
-    
-  }, 5000);
+
+
+}, 5000);
+
+/**
+ * Executing onLoad().
+ */
 onLoad();
-console.log("end");
+
