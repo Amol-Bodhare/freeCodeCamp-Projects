6ab8f2ea6d43aeea50ec86722d1ac9fe1b8a6afe
unknown <amolfeb95@gmail.com>
2018-04-27 14:31:48 -0400

    user story completed: Game restart

--- SimonGame/scripts/index.js
+++ SimonGame/scripts/index.js
@@ -1,52 +1,70 @@
 $(document).ready(function() {
     let order = [];
     let index1 = 0;
+    var c=0,c1=0;
     $('#startBtn').click(start);
     $('#but1').click(1,check);
     $('#but2').click(2,check);
     $('#but3').click(3,check);
     $('#but4').click(4,check);
+    $('#but1').prop('disabled',true);
+    $('#but2').prop('disabled',true);
+    $('#but3').prop('disabled',true);
+    $('#but4').prop('disabled',true);
     function start() {
-        
+        index1=0;
+        c1=0;
+        c=0;
+        $('#but1').prop('disabled',false);
+        $('#but2').prop('disabled',false);
+        $('#but3').prop('disabled',false);
+        $('#but4').prop('disabled',false);
         for(var i=0;i<20;i++) {
             let index = Randomize();
-            order.push({index: index,flag:false});
+            order[i]=index;
         }
         console.log(order);
 
         display(order[0],index1+1);
 
     }
-    var c=0,c1=0;
+    function stop() {
+        $('#but1').prop('disabled',true);
+        $('#but2').prop('disabled',true);
+        $('#but3').prop('disabled',true);
+        $('#but4').prop('disabled',true);
+    }
+    
     function display(num,count) {
         var y=setTimeout(function () {
-        $('#but'+num.index+'').delay(1000).css('color','red');
+            $('#status').html(`${index1+1}`);
+        $('#but'+num+'').delay(1000).css('color','red');
         },1000);
         var x=setTimeout(function () {
             
-            $('#but'+num.index+'').css('color','black');
+            $('#but'+num+'').css('color','black');
             if(c>=(count-1)){
-                console.log("clear");
+                console.log("clear:"+c);
                 clearTimeout(x);
                 c1=0;
-                clearOrder();
+                //clearOrder();
                 return;
             }
             c++;
             display(order[c],count);
         }, 2000);
     }
-    // display(order[0],index1+1);
-    var checkflag=false;
+    
     function check(event) {
         
-        if(event.data === order[c1].index) {
+        if(event.data === order[c1]) {
             console.log("right");
             c1++;
             
             
         } else {
             console.log('wrong step---- Listen again');
+            $('#status').html('Wrong step - try again');
             c=0;
             display(order[0],index1+1);
             
@@ -54,27 +72,38 @@ $(document).ready(function() {
             
         }
         if(c1===index1+1){
+            
             index1++;c=0;
-            display(order[0],index1+1);
-        }
-    }
-    function clearOrder() {
-        for(var i=0;i<=index1;i++) {
-            order[i].flag=false;
-        }
-    }
-    function checkSequence() {
-        var checksum = -1;
-        for(var i=0;i<=index1;i++){
-            if(order[i].flag == true) {
-                checksum++;
+            console.log("index is: "+index1)
+            if(index1<20) {
+                display(order[0],index1+1);
+            } else {
+                $('#status').html(`Victory`);
+                stop(); 
             }
         }
-        if(checksum === index1){
-            return true;
-        }
-        else return false;
     }
+    // function clearOrder() {
+    //     for(var i=0;i<=index1;i++) {
+    //         order[i].flag=false;
+    //     }
+    // }
+    // function checkSequence() {
+    //     var checksum = -1;
+    //     for(var i=0;i<=index1;i++){
+    //         if(order[i].flag == true) {
+    //             checksum++;
+    //         }
+    //     }
+    //     if(checksum === index1){
+    //         if(checksum === 19){
+                
+    //         } else {
+    //             return true;
+    //         }
+    //     }
+    //     else return false;
+    // }
     function Randomize() {
         return Math.floor(Math.random() * (4 - 1 + 1)) + 1;
     }
